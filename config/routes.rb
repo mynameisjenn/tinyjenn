124e0a7337cce1f8e69053cefb4e63b84e5f6a75
mynameisjenn <jenniferlynnhatton@gmail.com>
2018-07-23 17:35:19 -0500

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'posts#index'
+
+  resources :posts
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
 end
