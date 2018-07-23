7f4fa22d40c0afdd21b2416233292f13a2ed7725
mynameisjenn <jenniferlynnhatton@gmail.com>
2018-07-23 17:11:29 -0500

    added Posts controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Triumphs')
+Category.create(name: 'Today I Learned')
+Category.create(name: 'Code Challenges')
+Category.create(name: 'Forward Momentum')
+Category.create(name: 'Frustrations')
