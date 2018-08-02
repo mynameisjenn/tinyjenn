774592f8570ba3ca8618c28c004a2757a4bba7b5
mynameisjenn <jenniferlynnhatton@gmail.com>
2018-08-02 16:19:00 -0500

    added CRUD and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :category
+  has_many :line_items, inverse_of: :order
   validates :title, :content, :category_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
   root 'posts#index'
-
   resources :posts
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+  
 end
